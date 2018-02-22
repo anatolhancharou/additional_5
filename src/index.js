964b5863a613999360aff51f4cae8b14f00a4be1
Anatol Hancharou <a.potteroff@gmail.com>
2018-02-22 23:52:58 +0300

    additional5 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var result = str;
+  var count = str.length;
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    result = result.split(bracketsConfig[i][0] + bracketsConfig[i][1]).join('');
+    if (result.length < count) {
+      count = result.length;
+      i = -1;
+    }
+  }
+  return result.length === 0 ? true : false;
 }
